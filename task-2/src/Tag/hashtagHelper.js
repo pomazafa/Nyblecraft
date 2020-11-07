25e8320757307933f23d3ee2353f98c36d8af430
Polina <p.duben@mail.ru>
2020-11-07 16:51:12 +0300

    fix reqular expression

--- task-2/src/Tag/hashtagHelper.js
+++ task-2/src/Tag/hashtagHelper.js
@@ -1,5 +1,5 @@
 export default function findHashtags(searchText) {
-    var regexp = /(\s|^)#[\wа-яА-Я][\wа-яА-Я]+\b/gm
+    var regexp = /(\s|^)#[\wа-яА-Я][\wа-яА-Я]+/gm
     let result = searchText.match(regexp);
     if (result) {
         result = result.map(function(s){ return s.trim();});
